53f7f6ddae7f9a4ee035d9d3165804b032e6f702
donnak <donnak@DONNAKF359>
2024-01-17 23:25:08 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Donna Kim
+ * Section 3
+ * Description: Simulate rolls of two 6 sided dice. Take user input for the number of rolls to simulate. Show result.
+ */
+using MissionTwoDiceRoll;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numRolls = 0; // variable to store number of rolls
+        Console.WriteLine("Welcome to the dice throwing simulator!\n"); // Welcome user
+        Console.WriteLine("How many dice rolls would you like to simulate?"); // Ask for number of rolls
+        numRolls = int.Parse(Console.ReadLine()); // save number of rolls inputted by user
+
+        // check if they entered a valid number of rolls (must be 1 or above)
+        if (numRolls >= 1)
+        {
+            int[] results = SimulateRoll.Roll(numRolls); // roll the die and save the results from all rolls
+
+            // comments to show the user
+            Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS\nEach \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + numRolls + "\n");
+
+            // for loop to print the results
+            for (int i = 0; i < results.Length; i++)
+            {
+                // calculate percent rolled of total rolls
+                double doubleNumRolls = numRolls;
+                int percentRolled = Convert.ToInt32(Math.Round((results[i] / doubleNumRolls) * 100));
+
+                // output of line recording each total for possible roll number (2-12)
+                string resultOutput = (i + 2) + ": ";
+                // for loop to print out the correct number of astericks (one asterick = one percent)
+                for (int numAstericks = 0; numAstericks < percentRolled; numAstericks++)
+                {
+                    resultOutput = resultOutput + "*";
+                }
+
+                // print out the line
+                Console.WriteLine(resultOutput);
+            }
+        }
+        else
+        {
+            // if user inputs a number not 1 or higher
+            Console.WriteLine("\nYou entered an invalid number of rolls.");
+        }
+
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!"); // ending comment
+    }
+}
---
+++ SimulateRoll.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissionTwoDiceRoll
+{
+    internal class SimulateRoll
+    {
+        // method to roll dice
+        public static int[] Roll(int nRolls)
+        {
+            Random rnd = new Random(); // to produce random number
+
+            // index 0 is for a roll of 2, index 1 is for a roll of 3, index 2 is for a roll of 4, etc.
+            int[] rollsResult = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // array of ints to hold results
+
+            // for loop to roll the dice the amount of times the user inputs
+            for (int i = 0; i < nRolls; i++)
+            {
+                //generate random number from 1 through 6
+                int rollNumber1 = rnd.Next(1, 7); // first die
+                int rollNumber2 = rnd.Next(1, 7); // second die
+
+                int rollTotal = rollNumber1 + rollNumber2; // add two dice to get the total for this roll
+
+                rollsResult[rollTotal - 2] = rollsResult[rollTotal - 2] + 1; // add 1 to the total roll position in the array that keeps track of the result
+            }
+
+            return rollsResult; // return the array with the result of total rolls for each roll total (2-12)
+        }
+    }
+}
